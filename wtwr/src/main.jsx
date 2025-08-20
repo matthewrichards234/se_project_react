82db89f9fecbf062508424aae61f8ab456efa233
Matthew Richards <matthewrichards234@gmail.com>
2025-08-20 19:46:51 -0400

    keep main in src

--- wtwr/src/main.jsx
+++ wtwr/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App/App";
+import Header from "./components/Header/Header";
 
 createRoot(document.getElementById("root")).render(
   <div>
     <App />
+    <Header />
   </div>
 );
