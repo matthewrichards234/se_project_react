ac79de0eda4828582f8c6944fd5a8694cb8ae22f
Matthew Richards <matthewrichards234@gmail.com>
2025-08-20 19:49:19 -0400

    add footer

--- wtwr/src/main.jsx
+++ wtwr/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 createRoot(document.getElementById("root")).render(
   <div>
     <App />
     <Header />
+    <Footer />
   </div>
 );
