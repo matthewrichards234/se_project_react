015316ce038e74057391056673fdb966a3671c1b
Matthew Richards <matthewrichards234@gmail.com>
2025-09-10 18:12:04 -0400

    skeleton for add clothing on profile

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import "./ClothesSection.css";
 import ItemCards from "../ItemCards/ItemCards";
 
 const ClothesSection = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleChangeModal() {
+    setIsModalOpen(!isModalOpen);
+  }
   return (
     <div className="clothes-section">
-      <div className="clothes-section__container"></div>
+      <div className="clothes-section__container">
+        <p className="clothes-section__title">Your items</p>
+        <button type="button" className="clothes-section__add-new">
+          Add new
+        </button>
+        {/* AddItemModal logic here */}
+      </div>
       <ItemCards />
     </div>
   );
