5801ddc55fb5df98546bbd1db29c851f83d7236f
Matthew Richards <matthewrichards234@gmail.com>
2025-10-02 12:33:25 -0400

    feat: add register and login modals after refactor modalw/form for reusability

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import useForm from "../../hooks/useForm";
 
-const LoginModal = ({ isOpen, onClose, handleSubmit }) => {
-  return <div>LoginModal</div>;
+const LoginModal = ({ isOpen, onClose, handleOnSubmit }) => {
+  const { values, handleChange } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleOnSubmit(values);
+  };
+  return (
+    <ModalWithForm
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      title="Log In"
+      submitText="Log In"
+    >
+      <label htmlFor="email" className="modal__label">
+        Email
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        className="modal__input"
+        placeholder="Email"
+        required
+        onChange={handleChange}
+        value={values.email}
+      />
+      <label htmlFor="password" className="modal__label">
+        Password*
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        className="modal__input"
+        placeholder="Password"
+        required
+        onChange={handleChange}
+        value={values.password}
+      />
+    </ModalWithForm>
+  );
 };
 
 export default LoginModal;
--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import useForm from "../../hooks/useForm";
 
-const RegisterModal = ({ isOpen, onClose, handleSubmit }) => {
-  return <div>RegisterModal</div>;
+const RegisterModal = ({ isOpen, onClose, handleOnSubmit }) => {
+  const { values, handleChange } = useForm({
+    email: "",
+    password: "",
+    name: "",
+    avatarUrl: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleOnSubmit(values);
+  };
+
+  return (
+    <ModalWithForm
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      title="Sign Up"
+      submitText="Next"
+    >
+      <label htmlFor="email" className="modal__label">
+        Email*
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        className="modal__input"
+        placeholder="Email"
+        required
+        onChange={handleChange}
+        value={values.email}
+      />
+
+      <label htmlFor="password" className="modal__label">
+        Password*
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        className="modal__input"
+        placeholder="Password"
+        required
+        onChange={handleChange}
+        value={values.password}
+      />
+
+      <label htmlFor="name" className="modal__label">
+        Name
+      </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        className="modal__input"
+        placeholder="Name"
+        required
+        onChange={handleChange}
+        value={values.name}
+      />
+
+      <label htmlFor="avatarUrl" className="modal__label">
+        Avatar URL
+      </label>
+      <input
+        type="url"
+        id="avatarUrl"
+        name="avatarUrl"
+        className="modal__input"
+        placeholder="Avatar URL"
+        required
+        minLength={2}
+        onChange={handleChange}
+        value={values.avatarUrl}
+      />
+    </ModalWithForm>
+  );
 };
 
 export default RegisterModal;
