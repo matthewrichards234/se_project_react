dcbfe77f614c131238b5071fc2db9aa99e42f576
Matthew Richards <matthewrichards234@gmail.com>
2025-10-05 11:36:57 -0400

    basic logic of edit prof modal

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import "./SideBar.css";
 import PFP from "../../assets/Images/user-pfp.svg";
+import EditProfileModal from "../EditProfileModal/EditProfileModal";
 
 const SideBar = ({
   handleOpenModal,
@@ -12,7 +13,11 @@ const SideBar = ({
         <img src={PFP} alt="PFP" className="sidebar__pfp" />
         <p className="sidebar__username">Terrence Tegegne</p>
       </div>
-      <button type="button" className="sidebar__edit-profile-btn">
+      <button
+        type="button"
+        className="sidebar__edit-profile-btn"
+        onClick={EditProfileModal}
+      >
         Change profile data
       </button>
       <button type="button" className="sidebar__logout-btn">
